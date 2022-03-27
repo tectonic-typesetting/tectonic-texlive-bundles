0d87176ae845ee9dff30e4e492aa749263ed82b1
Peter Williams <peter@newton.cx>
2022-03-27 09:25:46 -0400

    tests/classes.py: reformat with "black"

--- tests/classes.py
+++ tests/classes.py
@@ -16,21 +16,21 @@ from test_utils import *
 
 # We use percent formatting since all the TeX braces would be super annoying to
 # escape in str.format() formatting.
-DOC_CLASS_TEMPLATE = r'\documentclass{%(class)s}'
+DOC_CLASS_TEMPLATE = r"\documentclass{%(class)s}"
 
-TITLE_AUTHOR = r'''\title{Test Title}
-\author{An Author}'''
+TITLE_AUTHOR = r"""\title{Test Title}
+\author{An Author}"""
 
-DOCUMENT_BODY = r'''\begin{document}
+DOCUMENT_BODY = r"""\begin{document}
 Hello, world.
-\end{document}'''
+\end{document}"""
 
 
 def entrypoint(argv):
     settings = make_arg_parser().parse_args(argv[1:])
     bundle = Bundle.open_with_inferred_state(settings.bundle_dir)
 
-    classdir = bundle.test_path('classes')
+    classdir = bundle.test_path("classes")
     n_errors = 0
     n_surprises = 0
     n_tested = 0
@@ -45,97 +45,97 @@ def entrypoint(argv):
     with open(bundle.listing_path()) as flist:
         for line in flist:
             base = line.strip()
-            if base.endswith('.cls'):
+            if base.endswith(".cls"):
                 bundle_classes.add(base[:-4])
 
     # Compare to the test reference data
 
     ref_classes = {}
 
-    with open(bundle.path('classes.txt')) as fref:
+    with open(bundle.path("classes.txt")) as fref:
         for line in fref:
             bits = line.split()
             classname = bits[0]
-            tags = bits[1].split(',')
+            tags = bits[1].split(",")
             ref_classes[classname] = tags
 
     # Check that those lists agree
 
     for c in bundle_classes:
         if c not in ref_classes:
-            print(f'MISSING {c} - not in classes.txt')
+            print(f"MISSING {c} - not in classes.txt")
             n_missing += 1
             n_errors += 1
 
     for c in ref_classes.keys():
         if c not in bundle_classes:
-            print(f'REMOVED {c} - in classes.txt but not bundle')
+            print(f"REMOVED {c} - in classes.txt but not bundle")
             n_removed += 1
             n_errors += 1
 
     # Run the tests
 
     for cls, flags in ref_classes.items():
-        print(cls, '... ', end='')
+        print(cls, "... ", end="")
         n_tested += 1
 
         thisdir = os.path.join(classdir, cls)
         os.makedirs(thisdir, exist_ok=True)
 
-        texpath = os.path.join(thisdir, 'index.tex')
+        texpath = os.path.join(thisdir, "index.tex")
 
         params = {
-            'class': cls,
+            "class": cls,
         }
 
-        with open(texpath, 'wt') as f:
+        with open(texpath, "wt") as f:
             print(DOC_CLASS_TEMPLATE % params, file=f)
 
-            if 'titleauth' in flags:
+            if "titleauth" in flags:
                 print(TITLE_AUTHOR, file=f)
 
             print(DOCUMENT_BODY, file=f)
 
-        with open(os.path.join(thisdir, 'log.txt'), 'wb') as log:
+        with open(os.path.join(thisdir, "log.txt"), "wb") as log:
             result = subprocess.call(
-                [TECTONIC_PROGRAM, '-p', '-b', bundle.zip_path(), texpath],
-                shell = False,
-                stdout = log,
-                stderr = subprocess.STDOUT,
+                [TECTONIC_PROGRAM, "-p", "-b", bundle.zip_path(), texpath],
+                shell=False,
+                stdout=log,
+                stderr=subprocess.STDOUT,
             )
 
         if result == 0:
-            if 'ok' in flags:
-                print('pass')
+            if "ok" in flags:
+                print("pass")
             else:
                 # This test succeeded even though we didn't expect it to.
                 # Not a bad thing, but worth noting!
-                print('pass (unexpected)')
+                print("pass (unexpected)")
                 n_surprises += 1
         else:
-            if 'xfail' in flags:
-                print('xfail')
+            if "xfail" in flags:
+                print("xfail")
                 n_xfail += 1
             else:
                 # This test failed unexpectedly :-(
-                print('FAIL')
+                print("FAIL")
                 n_errors += 1
 
     print()
-    print('Summary:')
-    print(f'- Tested {n_tested} documentclasses')
+    print("Summary:")
+    print(f"- Tested {n_tested} documentclasses")
     if n_missing:
-        print(f'- {n_missing} documentclasses missing from classes.txt')
+        print(f"- {n_missing} documentclasses missing from classes.txt")
     if n_removed:
-        print(f'- {n_removed} documentclasses in classes.txt removed from bundle')
+        print(f"- {n_removed} documentclasses in classes.txt removed from bundle")
     if n_xfail:
-        print(f'- {n_xfail} expected failures')
+        print(f"- {n_xfail} expected failures")
     if n_surprises:
-        print(f'- {n_surprises} surprise passes')
+        print(f"- {n_surprises} surprise passes")
     if n_errors:
-        print(f'- {n_errors} total errors: test failed (see outputs in {classdir})')
+        print(f"- {n_errors} total errors: test failed (see outputs in {classdir})")
     else:
-        print(f'- no errors: test passed (outputs stored in {classdir})')
+        print(f"- no errors: test passed (outputs stored in {classdir})")
 
     return 1 if n_errors else 0
 
@@ -149,5 +149,5 @@ def make_arg_parser():
     return p
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(entrypoint(sys.argv))
