76623973ae7fc289b7bf90eb8910c09d79efbd88
rm-dr <96270320+rm-dr@users.noreply.github.com>
2023-10-23 10:40:30 -0700

    Renamed bundles

--- bundles/full/bundle.sh
+++
@@ -1,6 +0,0 @@
-bn_name="full"
-bn_texlive_version="2023.0r0"
-
-# Get this by running `sha256 -b texlive.iso`.
-# Do not include the file name.
-bn_texlive_hash="620923de5936ab315926e81de2cb8253a9c626fb7e03d8ffe0d424598eb32f94"
---
+++ bundles/texlive2022.0r0/bundle.sh
@@ -0,0 +1,6 @@
+bundle_name="texlive2022.0r0"
+
+# Compute the hash by running `sha256 -b texlive.iso`.
+# Do not include the file name in budle_texlive_hash.
+bundle_texlive_file="texlive2022.iso"
+bundle_texlive_hash="c2576926da862e4bf4f4e699fb6421e8949dc75b7f22cde8b1154361b963692e"
