d75989f375c8e6c7586915bda3c7fc8b22dead50
rm-dr <96270320+rm-dr@users.noreply.github.com>
2023-10-23 10:17:58 -0700

    Major refactor of make-zipfile.py

--- docker-image/scripts/make-zipfile.py
+++ docker-image/scripts/make-zipfile.py
@@ -8,52 +8,30 @@ installation.
 """
 
 import argparse
-import os.path
 import sys
+import os
 import zipfile
-
-def warn(text):
-    print("warning:", text, file=sys.stderr)
-
-
-def die(text):
-    raise SystemExit(f"error: {text}")
+import hashlib
+import struct
+from pathlib import Path
 
 
-def chown_host(path, recursive=True):
-    uid = int(os.environ["HOSTUID"])
-    gid = int(os.environ["HOSTGID"])
+# Bundle parameters
+NAME = os.environ["bn_name"]
+VERSION = os.environ["bn_texlive_version"]
 
-    os.chown(path, uid, gid)
+# Input paths
+PATH_ignore  = Path("/bundle/ignore")
+PATH_extras  = Path("/bundle/extras")
+PATH_patched = Path("/bundle/patched")
+PATH_texlive = Path("/install/texmf-dist")
 
-    if not recursive:
-        return
+# Output paths
+PATH_clash   = Path("/output/clash-report.txt")
+PATH_zip     = Path(f"/output/{NAME}-{VERSION}.zip")
+PATH_hash    = Path(f"/output/{NAME}-{VERSION}.sha256sum")
+PATH_listing = Path(f"/output/{NAME}-{VERSION}.listing.txt")
 
-    for dirpath, dirnames, filenames in os.walk(path):
-        for dname in dirnames:
-            os.lchown(os.path.join(dirpath, dname), uid, gid)
-
-        for fname in filenames:
-            os.lchown(os.path.join(dirpath, fname), uid, gid)
-
-
-IGNORED_BASE_NAMES = set([
-    "00readme.txt",
-    "LICENSE.md",
-    "Makefile",
-    "README",
-    "README.md",
-    "ls-R",
-])
-
-IGNORED_EXTENSIONS = set([
-    "fmt",
-    "log",
-    "lua",
-    "mf",
-    "pl",
-    "ps",
-])
 
 
 class ZipMaker(object):
@@ -61,60 +39,40 @@ class ZipMaker(object):
         self.zip = zip
         self.item_shas = {}
         self.final_hexdigest = None
-        self.clashes = {}  # basename => {digest => fullpath}
 
-        self.ignored_tex_paths = set()
-
-        with open("/bundle/ignored-tex-paths.txt", "r") as f:
-            for line in f:
-                line = line.split("#")[0].strip()
-                if len(line):
-                    self.ignored_tex_paths.add(line)
-
-        self.ignored_tex_path_prefixes = []
-
-        with open("/bundle/ignored-tex-path-prefixes.txt", "r") as f:
-            for line in f:
-                line = line.split("#")[0].strip()
-                if len(line):
-                    self.ignored_tex_path_prefixes.append(line)
+        # Keeps track of conflicting file names
+        # { "basename": {
+        #       b"digest": Path(fullpath)
+        # }}
+        self.clashes = {}
 
 
-    def consider_file(self, tex_path, base_name):
+        # Load ignore patterns
+        self.ignore_patterns = set()
+        if PATH_ignore.is_file():
+            with PATH_ignore.open("r") as f:
+                for line in f:
+                    line = line.split("#")[0].strip()
+                    if len(line):
+                        self.ignore_patterns.add(line)
+
+
+    def consider_file(self, file):
         """
         Consider adding the specified TeXLive file to the installation tree.
         This is where all the nasty hairy logic will accumulate that enables us
         to come out with a nice pretty tarball in the end.
         """
 
-        if base_name in IGNORED_BASE_NAMES:
-            return False
-
-        ext_bits = base_name.split(".", 1)
-        if len(ext_bits) > 1 and ext_bits[1] in IGNORED_EXTENSIONS:
-            return False
-
-        if tex_path in self.ignored_tex_paths:
-            return False
-
-        for pfx in self.ignored_tex_path_prefixes:
-            if tex_path.startswith(pfx):
+        for pattern in self.ignore_patterns:
+            if file.relative_to(PATH_texlive).match(pattern):
                 return False
 
         return True
 
 
-    def _walk_onerr(self, oserror):
-        warn(f"error navigating installation tree: {oserror}")
-
-
-    # Actually building the full Zip
-
-    def add_file(self, full_path):
-        base = os.path.basename(full_path)
-
+    def add_file(self, full_path: Path):
         # Get the digest
-
         with open(full_path, "rb") as f:
             contents = f.read()
 
@@ -122,17 +80,15 @@ class ZipMaker(object):
         s.update(contents)
         digest = s.digest()
 
-        # OK, have we seen this before?
-
-        prev_tuple = self.item_shas.get(base)
-
+        # Have we seen this filename before?
+        prev_tuple = self.item_shas.get(full_path.name)
         if prev_tuple is None:
-            # New basename, yay
-            self.zip.writestr(base, contents)
-            self.item_shas[base] = (digest, full_path)
+            # This is a new file, ok for now.
+            self.zip.writestr(full_path.name, contents)
+            self.item_shas[full_path.name] = (digest, full_path)
         elif prev_tuple[0] != digest:
-            # Already seen basename, and new contents :-(
-            bydigest = self.clashes.setdefault(base, {})
+            # We already have a file with this name and different contents
+            bydigest = self.clashes.setdefault(full_path.name, {})
 
             if not len(bydigest):
                 # If this is the first duplicate, don't forget that we've seen
@@ -143,36 +99,36 @@ class ZipMaker(object):
             pathlist.append(full_path)
 
 
-    def go(self):
 
+
+    def go(self):
         # Add the extra files preloaded in the bundle
-        for name in os.listdir("/bundle/extras"):
-            self.add_file(os.path.join("/bundle/extras", name))
+        for f in PATH_extras.iterdir():
+            self.add_file(f)
 
         # Add the patched files, and make sure not to overwrite them later.
         patched_basenames = set()
-        for name in os.listdir("/bundle/patched"):
-            self.add_file(os.path.join("/bundle/patched", name))
-            patched_basenames.add(name)
+        for f in PATH_patched.iterdir():
+            self.add_file(f)
+            patched_basenames.add(f.name)
 
         # Add the main tree.
+        ignored = 0
+        print(f"Zipping {PATH_texlive}...")
+        for f in PATH_texlive.rglob("*"):
+            if not f.is_file():
+                continue
+            if f.name in patched_basenames:
+                continue
 
-        p = os.path.join("/install", "texmf-dist")
-        n = len(p) + 1
-        print(f"Scanning {p} ...")
+            if self.consider_file(f):
+                self.add_file(f)
+            else:
+                ignored += 1
 
-        for dirpath, _, filenames in os.walk(p, onerror=self._walk_onerr):
-            for fn in filenames:
-                if fn in patched_basenames:
-                    continue
-
-                full = os.path.join(dirpath, fn)
-                tex = full[n:]
-                if self.consider_file(tex, fn):
-                    self.add_file(full)
+        print(f"Done, ignored {ignored} files.")
 
         # Compute a hash of it all.
-
         print("Computing final hash ...")
         s = hashlib.sha256()
         s.update(struct.pack(">I", len(self.item_shas)))
@@ -187,30 +143,26 @@ class ZipMaker(object):
         self.zip.writestr("SHA256SUM", self.final_hexdigest)
 
         # Report clashes if needed
-
         if len(self.clashes):
-            warn(f"{len(self.clashes)} clashing basenames were observed")
+            print(f"WARNING: {len(self.clashes)} file clashes were found.")
+            print(f"Logging clash report to {PATH_clash}")
 
-            report_path = "/output/clash-report.txt"
-            warn(f"logging clash report to {report_path}")
-
-            with open(report_path, "wt") as f:
-                for base in sorted(self.clashes.keys()):
-                    print(f"{base}:", file=f)
-                    bydigest = self.clashes[base]
+            with PATH_clash.open("w") as f:
+                for filename in sorted(self.clashes.keys()):
+                    f.write(f"{filename}:\n")
+                    bydigest = self.clashes[filename]
 
                     for digest in sorted(bydigest.keys()):
-                        print(f"  {digest.hex()[:8]}:", file=f)
+                        f.write(f"\t{digest.hex()[:8]}:\n")
 
-                        for full in sorted(bydigest[digest]):
-                            print(f"     {full[n:]}", file=f)
+                        for path in sorted(bydigest[digest]):
+                            f.write(f"\t\t{path}\n")
+                    f.write("\n\n")
 
-            chown_host(report_path)
 
-
-    def write_listing(self, stream):
+    def write_listing(self, file):
         for base in sorted(self.item_shas.keys()):
-            print(base, file=stream)
+            file.write(base+"\n")
 
 
 def make_arg_parser():
@@ -229,31 +181,24 @@ def entrypoint(argv):
     version = os.environ["bn_texlive_version"]
 
     try:
-        zip_path = f"/output/{name}-{version}.zip"
-        paths.append(zip_path)
+        paths.append(PATH_zip)
 
-        with zipfile.ZipFile(zip_path, "w", zipfile.ZIP_DEFLATED, True) as zip:
+        with zipfile.ZipFile(PATH_zip, "w", zipfile.ZIP_DEFLATED, True) as zip:
             b = ZipMaker(zip)
             b.go()
 
         print("Final SHA256SUM:", b.final_hexdigest)
 
-        digest_path = f"/output/{name}-{version}.sha256sum"
-        print(f"Creating digest file in {digest_path}")
-        paths.append(digest_path)
+        print(f"Creating digest file in {PATH_hash}")
+        paths.append(PATH_hash)
+        with PATH_hash.open("w") as f:
+            f.write(b.final_hexdigest+"\n")
 
-        with open(digest_path, "wt") as f:
-            print(b.final_hexdigest, file=f)
-
-        listing_path = f"/output/{name}-{version}.listing.txt"
-        print(f"Creating listing file in {listing_path}")
-        paths.append(listing_path)
-
-        with open(listing_path, "wt") as f:
+        print(f"Creating listing file in {PATH_listing}")
+        paths.append(PATH_listing)
+        with PATH_listing.open("w") as f:
             b.write_listing(f)
 
-        for p in paths:
-            chown_host(p, recursive=False)
     except Exception as e:
         try:
             for p in paths:
