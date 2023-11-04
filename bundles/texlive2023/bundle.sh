684821dfb7d0d9175117c0c5562d2a7980117ff2
rm-dr <96270320+rm-dr@users.noreply.github.com>
2023-11-03 22:26:59 -0700

    Renamed texlive2023 bundle

--- bundles/texlive2023/bundle.sh
+++ bundles/texlive2023/bundle.sh
@@ -1,7 +1,7 @@
 # Built from texlive2023.0r0
 # Filename texlive2023.iso
 
-bundle_name="texlive2023-nopatch"
+bundle_name="texlive2023"
 bundle_faketime="2023-10-10 00:00:00"
 bundle_texlive_hash="620923de5936ab315926e81de2cb8253a9c626fb7e03d8ffe0d424598eb32f94"
 bundle_result_hash="209d4b6a220bec2d1e2e89c7ba0dbe02b0e6f2416abce5fb8df228e06cf1e335"
