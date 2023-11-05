2e0bfde46728832e7183cfe1d30dfc9d73159055
rm-dr <96270320+rm-dr@users.noreply.github.com>
2023-11-05 11:02:34 -0800

    Fixed ignore patterns

--- bundles/texlive2023/bundle.sh
+++ bundles/texlive2023/bundle.sh
@@ -4,4 +4,4 @@
 bundle_name="texlive2023"
 bundle_faketime="2023-10-10 00:00:00"
 bundle_texlive_hash="620923de5936ab315926e81de2cb8253a9c626fb7e03d8ffe0d424598eb32f94"
-bundle_result_hash="209d4b6a220bec2d1e2e89c7ba0dbe02b0e6f2416abce5fb8df228e06cf1e335"
+bundle_result_hash="131f483ff31f12b2ca5e7139fb6c5040513b1975c769787221dcf396ba737065"
